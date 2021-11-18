35b23b7669de84b69195b138a9e366a6b16993ee
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2021-11-18 17:57:40 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Ridhhi',
+        username: 'Amazon'
+    },
+    {
+        id: 2,
+        name: 'Sahil',
+        username: 'Tower Research'
+    }
+];
+
+export default userList;
